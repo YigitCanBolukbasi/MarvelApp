205253076a92e66f3be78ce2ad4f7b80e5f7275b
Yigit Can <y.can.bolukbasi@gmail.com>
2021-12-21 18:02:42 +0300

    Button component implemented

---
+++ src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text,TouchableOpacity } from "react-native";
+
+import styles from "./Button.styles";
+
+function Button({label, theme= 'default', ...otherProps}) {
+    return(
+        <TouchableOpacity style={styles[theme].container} {...otherProps}>
+          <Text style={styles[theme].label}>{label}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default Button;
