60c9fe31ad40c6e9ccb01609795a03870a5cc2ee
Yigit Can <y.can.bolukbasi@gmail.com>
2021-12-21 18:39:44 +0300

    Input component inplemented

---
+++ src/components/Input/Input.styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    margin: 5,
+  },
+  label: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  input_container: {
+    backgroundColor: '#e0e0e0',
+    padding: 5,
+    borderRadius: 5,
+  },
+  input: {},
+});
