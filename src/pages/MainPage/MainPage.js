65a7beb662a7c7bfc3a72d5557a9c9df23f634fa
Emre Samurlu <emrsmrl@gmail.com>
2021-12-22 17:46:03 +0300

    Main Page Implemented

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {FlatList, SafeAreaView} from 'react-native';
 import Config from 'react-native-config';
 
 import useFetch from '../../hooks/useFetch';
+import ComicCard from '../../components/ComicCard/ComicCard';
+import {useNavigation} from '@react-navigation/native';
 
 const MainPage = () => {
+  const navigation = useNavigation();
   const {data} = useFetch(
-    `${Config.API_URL}/comics?ts=1&apikey=${Config.API_KEY}&hash=${Config.API_HASH}`,
+    `${Config.API_URL}/comics?format=comic&formatType=comic&ts=1&apikey=${Config.API_KEY}&hash=${Config.API_HASH}`,
   );
-  console.log(data);
+  // const {data: characters} = useFetch(
+  //   `${Config.API_URL}/characters?comics=1749&ts=1&apikey=2bf3b8fe6dedde5d3df4920e6df21214&hash=3daa12a3fa29fab4e305a83ef7ef09ec`,
+  // );
+
+  const handleComicDetail = id => {
+    navigation.navigate('DetailPage', {id});
+  };
+
+  const renderComicCard = ({item}) => (
+    <ComicCard comic={item} onPress={handleComicDetail} />
+  );
   return (
     <SafeAreaView>
-      <Text>Main Page</Text>
+      <FlatList data={data} renderItem={renderComicCard} />
     </SafeAreaView>
   );
 };
