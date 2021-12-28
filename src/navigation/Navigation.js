f34d4c1f7afbbf3735d7b3851794914674a2ea99
Yigit Can <y.can.bolukbasi@gmail.com>
2021-12-28 16:43:35 +0300

    Routers Implemented

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, DarkTheme} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import StackNavigation from './StackNavigation/StackNavigation';
 import FavoritesPage from '../pages/FavoritesPage';
 import CharactersPage from '../pages/CharactersPage';
+import routes from './routes';
 
 const Tab = createBottomTabNavigator();
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={DarkTheme}>
       <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name="mainpage" component={StackNavigation} />
-        <Tab.Screen name="Search" component={CharactersPage} />
-        <Tab.Screen name="Fav" component={FavoritesPage} />
+        <Tab.Screen name={routes.Main_page} component={StackNavigation} />
+        <Tab.Screen name={routes.Search_page} component={CharactersPage} />
+        <Tab.Screen name={routes.Fav_Page} component={FavoritesPage} />
       </Tab.Navigator>
     </NavigationContainer>
   );
